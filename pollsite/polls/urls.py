0eefc7b469dc7ce0fcb737e517a8ddbcff516cb8
Alwoch <salwoch@gmail.com>
2023-01-31 13:02:07 +0300

    create poll views

--- pollsite/polls/urls.py
+++ pollsite/polls/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 
 from . import views
 
+# app_name = 'polls' #differentiate the urls in polls from those in other apps
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote')
+# ]
+
+#translating the above url patterns to generic views
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,6 +1,67 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.urls import reverse
+from .models import Question,Choice
+from django.views import generic
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# index view
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     } #context is a dictionary mapping template variable names to python objects
+#     return render(request,'polls/index.html',context)
+
+# # detail view
+# def detail(request, question_id):
+#     #question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+#results view
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+#Django generic views
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# # vote view
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        #The reverse() function prevents us from having to hardcode a a full url path
+        #The redirect happens with the question_id as part of the url
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
